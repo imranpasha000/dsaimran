95cd9e5e29e76910f3149598f87a1ee71e3b161e
MOHAMMAD IMRAN <pashaturki19081@gmail.com>
2023-02-11 12:59:25 +0530

    Add files via upload

---
+++ .GCD hcf.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	
+	int result = min(a,b);
+	while(result>0){
+		if(a%result == 0 && b% result == 0){
+			break;
+		}
+	result--;
+	}
+	return  result;
+}
+
+int main()
+{
+
+
+int a =  16, b = 34;
+cout<<"GCD of" << a <<  "and" << b << "is" << gcd(a,b);
+return 0;
+}
